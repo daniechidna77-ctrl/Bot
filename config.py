e395572993d6e9ecd6ad7c54455124b9237920d7
daniechidna77-ctrl <daniechidna77@gmail.com>
2026-08-07 15:34:11 +0330

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,12 @@ import os
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 ADMIN_ID = int(os.environ.get("ADMIN_ID", 8255361263))
 
+# کانال‌های پیش‌فرض (همینجا اضافه کن)
+DEFAULT_CHANNELS = [
+    "melorina77",
+    "Yuriii789",
+    "yuriii42"
+]
+
 if not BOT_TOKEN:
     raise ValueError("❌ BOT_TOKEN تنظیم نشده!")
