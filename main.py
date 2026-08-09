71d3e310cb7199c21252079d8d0ff023df504726
daniechidna77-ctrl <daniechidna77@gmail.com>
2026-08-09 16:51:44 +0330

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,17 @@ import asyncio
 import logging
 from aiogram import Bot, Dispatcher
 from config import BOT_TOKEN
-from handlers import router
+from handlers import router as user_router
+from admin_panel import router as admin_router
 
 logging.basicConfig(level=logging.INFO)
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
-dp.include_router(router)
+
+# ثبت روت‌ها
+dp.include_router(user_router)
+dp.include_router(admin_router)
 
 async def main():
     print("🤖 ربات روشن شد!")
